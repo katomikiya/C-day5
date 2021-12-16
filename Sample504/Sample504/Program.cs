109bf17e5d459159895f5e88620b3e8496c1e2d8
katomikiya <kato-m@omoteya.co.jp>
2021-12-16 17:42:22 +0900

    foreach文とfor文の比較

---
+++ Sample504/Sample504/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample504
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6 };
+            foreach (int i in nums)     // for(int i = 0; i < nums.Length; i++)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write("{0} ", nums[i]);
+            }
+        }
+    }
+}
